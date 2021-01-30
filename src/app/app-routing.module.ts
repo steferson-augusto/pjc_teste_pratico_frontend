86bf0b856a14fa146bac66fe3ee4897da5e209d0
Steferson Augusto <steferson_a@hotmail.com>
2021-01-29 23:02:24 -0400

    feat: adiciona e configura rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = []
+import { AlbumsComponent } from './pages/albums/albums.component'
+import { ArtistsComponent } from './pages/artists/artists.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { UsersComponent } from './pages/users/users.component'
+import { NavbarComponent } from './components/template/navbar/navbar.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NavbarComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'artists', component: ArtistsComponent },
+      { path: 'albums', component: AlbumsComponent }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppRoutingModule } from './app-routing.module'
 import { MaterialModule } from './material.module'
 import { AppComponent } from './app.component'
 import { NavbarComponent } from './components/template/navbar/navbar.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { UsersComponent } from './pages/users/users.component'
+import { ArtistsComponent } from './pages/artists/artists.component'
+import { AlbumsComponent } from './pages/albums/albums.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    UsersComponent,
+    ArtistsComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule,
