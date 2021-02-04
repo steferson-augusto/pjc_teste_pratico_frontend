18d34565c96af7102f356a00a5f98380c4c12208
Steferson Augusto <steferson_a@hotmail.com>
2021-02-04 15:26:05 -0400

    feat: cria módulo para componentes comuns

---
+++ src/app/components/template/template.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { MaterialModule } from 'src/app/material/material.module'
+import { LoadingComponent } from '../template/loading/loading.component'
+import { CustomSnackbarService } from './custom-snackbar/custom-snackbar.service'
+import { CustomSnackbarComponent } from './custom-snackbar/custom-snackbar.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { RouterModule } from '@angular/router'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  declarations: [
+    CustomSnackbarComponent,
+    LoadingComponent,
+    NavbarComponent
+  ],
+  exports: [
+    CustomSnackbarComponent,
+    LoadingComponent,
+    NavbarComponent
+  ],
+  providers: [
+    CustomSnackbarService
+  ]
+})
+export class TemplateModule { }
