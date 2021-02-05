0b5c70ad0ff208468443c83af17b86121ddd180f
Steferson Augusto <steferson_a@hotmail.com>
2021-02-05 01:05:49 -0400

    fix: corrige interface de álbuns

--- src/app/components/album/album.model.ts
+++ src/app/components/album/album.model.ts
@@ -1,9 +1,11 @@
 /* eslint-disable camelcase */
+import { Artist } from '../artist/artist.model'
 import { Image } from '../image/image.model'
 
 export interface Album {
   id?: number
   name: string
+  artist?: string | Artist
   artist_id?: number
   year?: number
   images?: Image[]
