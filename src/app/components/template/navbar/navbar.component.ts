b813cd671b2f2a06a129c04e8e479feb8d0a0c19
Steferson Augusto <steferson_a@hotmail.com>
2021-02-05 11:40:01 -0400

    feat: adiciona botão de logout

--- src/app/components/template/navbar/navbar.component.ts
+++ src/app/components/template/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
+import { AuthService } from 'src/app/pages/auth/auth.service'
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +9,21 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
 })
 export class NavbarComponent {
   title: string = ''
-  constructor (private route: ActivatedRoute, private router: Router) {
+  constructor (
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         const data = this.route?.snapshot?.firstChild?.firstChild?.firstChild?.data
         this.title = data?.title ?? '404'
       }
     })
+  }
+
+  logout () {
+    this.authService.logout()
+    this.router.navigate(['/login'])
   }
 }
