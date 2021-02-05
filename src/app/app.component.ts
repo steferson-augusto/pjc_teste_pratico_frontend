3dc279471ffa5ec20eb4324a4ffac05fc1375cab
Steferson Augusto <steferson_a@hotmail.com>
2021-02-05 09:09:40 -0400

    refactor: remove atributos/métodos não utilizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pjc-frontend';
-}
+export class AppComponent { }
--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.scss']
 })
-export class AlbumsComponent implements OnInit {
-  constructor () { }
-
-  ngOnInit (): void {
-  }
-}
+export class AlbumsComponent { }
