7a6c9bf808e1a9118d9fb71e2eb5559efcde56a5
Steferson Augusto <steferson_a@hotmail.com>
2021-02-05 11:03:50 -0400

    test: adiciona módulo de roteador

--- src/app/components/profile/profile-edit/profile.component.spec.ts
+++ src/app/components/profile/profile-edit/profile.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterTestingModule } from '@angular/router/testing'
 import { MaterialModule } from 'src/app/material/material.module'
 import { ProfileService } from '../profile.service'
 
@@ -13,7 +14,7 @@ describe('ProfileEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, HttpClientTestingModule, MaterialModule, BrowserAnimationsModule],
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule, MaterialModule, BrowserAnimationsModule],
       providers: [ProfileService],
       declarations: [ProfileEditComponent]
     })
