a32e04205e27ba37087d41930bef73d4506a44e2
Steferson Augusto <steferson_a@hotmail.com>
2021-02-05 01:49:12 -0400

    test: cria testes para service de imagens

--- src/app/components/image/image.service.spec.ts
+++ src/app/components/image/image.service.spec.ts
@@ -1,16 +1,61 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
-import { ImageService } from './image.service';
+import { ImageService } from './image.service'
+import { ImageRequest } from './image.model'
 
 describe('ImageService', () => {
-  let service: ImageService;
+  let service: ImageService
+  let httpMock: HttpTestingController
+  let files: Set<File>
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ImageService);
-  });
+    TestBed.configureTestingModule({
+      providers: [ImageService],
+      imports: [HttpClientTestingModule]
+    })
+    service = TestBed.inject(ImageService)
+    httpMock = TestBed.inject(HttpTestingController)
+    const token = { type: 'bearer', token: 'token', refreshToken: 'refresh' }
+    window.localStorage.setItem('@TESTE-PRATICO:token', JSON.stringify(token))
+  })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  afterEach(() => {
+    try {
+      httpMock.verify()
+      window.localStorage.removeItem('@TESTE-PRATICO:token')
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  it('Deve ser criado', () => {
+    expect(service).toBeTruthy()
+  })
+
+  it('Deve fazer upload de imagens', () => {
+    const fake: ImageRequest = { message: 'Salvo com sucesso' }
+
+    files = new Set()
+
+    service.upload(files, 8).subscribe(result => {
+      expect(result).toEqual(fake)
+    })
+
+    const request = httpMock.expectOne(service.baseUrl)
+    expect(request.request.method).toBe('POST')
+    request.flush(fake)
+  })
+
+  it('Deve solicitar a remoção de uma imagem', () => {
+    const fake: ImageRequest = { message: 'Imagem apagada com sucesso' }
+
+    service.delete(8).subscribe(result => {
+      expect(result).toEqual(fake)
+    })
+
+    const request = httpMock.expectOne(`${service.baseUrl}/8`)
+    expect(request.request.method).toBe('DELETE')
+    request.flush(fake)
+  })
+})
